d1e140232751c7d3b6aa31d6913eda71a337edd1
elies-billotta <elies.billotta@gmail.com>
2024-03-05 10:30:54 +0100

    💄 ImGUI

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <imgui.h>
 #include <cstdlib>
 #include <vector>
 #define DOCTEST_CONFIG_IMPLEMENT
@@ -6,10 +7,10 @@
 #include "p6/p6.h"
 
 const int N = 100;
-const float areaSize = 0.8f;
 
 int main()
 {
+    auto areaSize = 0.8f;
     // Run the tests
     if (doctest::Context{}.run() != 0)
         return EXIT_FAILURE;
@@ -17,22 +18,35 @@ int main()
     // Actual application code
     auto ctx = p6::Context{{.title = "Boids will be boids"}};
     ctx.maximize_window();
-    Boid boid1 = Boid(0.0f, 0.0f, 0.00001f, 0.00001f);
+    Boid        boid1       = Boid(0.0f, 0.0f, 0.00001f, 0.00001f);
+    const char* items[]     = {"Cohesion", "", "Option 3"};
+    int         currentItem = 0;
 
     std::vector<Boid> boids;
+    std::string       text       = "Hello";
+    ImVec4            namedColor = ImVec4(0.4f, 0.7f, 0.0f, 1.0f);
+
+    ctx.imgui = [&]() {
+        ImGui::Begin("Test");
+        ImGui::SliderFloat("Square size", &areaSize, 0.f, 1.f);
+        ImGui::InputText("Text", &text);
+        ImGui::ColorPicker4("Color", (float*)&namedColor);
+        ImGui::Combo("##combo", &currentItem, items, IM_ARRAYSIZE(items));
+        ImGui::End();
+        // ImGui::ShowDemoWindow();
+    };
 
     for (int i = 0; i < N; i++)
     {
         boids.emplace_back(0.0f, 0.0f, 0.01f, 0.01f);
     }
     ctx.update = [&]() {
-        ctx.background(p6::NamedColor::Red);
+        ctx.background(p6::Color(namedColor.x, namedColor.y, namedColor.z, namedColor.w));
         ctx.square(p6::Center(0.0f, 0.0f), p6::Radius(areaSize));
         for (Boid& b : boids)
         {
             b.update();
             b.draw(ctx, areaSize);
-            
         }
     };
     ctx.start();
